64d9aa1862dd69372ee92b71ae9046a83b4c275d
CharChampion <charvikpenjarla@gmail.com>
2025-08-03 09:49:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# # Write a program to find the sum of natural numbers.
-# i=1
-# s = 0
-# while i<4:
-#     s=s+i
-#     i=i+1
-#     print(s)
-a = 0
-for i in range(0,5):
+# # # Write a program to find the sum of natural numbers.
+i=1
+s = 0
+while i<100:
+    s=s+i
+    i=i+1
+    print(s)
+# a = 0
+# for i in range(0,5):
     
-    a = a+i
-print(a)
+#     a = a+i
+# print(a)
